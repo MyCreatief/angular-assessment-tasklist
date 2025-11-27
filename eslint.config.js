332d5f9b6c73bcf4c8e9faf75141f93747c2c59d
Mylene <myklijn@gmail.com>
2025-11-27 14:08:04 +0100

    feat: initial Angular 17 setup
    Set up Angular 17 project with standalone components, Vitest testing, ESLint flat config,
    Prettier formatting, Husky pre-commit hook and lint-staged. Includes working TestBed config
    and testing environment fixes.

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  // TypeScript + Angular
+  {
+    files: ['**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettier,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+
+  // Templates
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      prettier,
+    ],
+    rules: {},
+  }
+);
